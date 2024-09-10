d019d293eb4abf6a55f62a30be9d09c47f2a7241
Anand-Anathur-Elangovan <anandanathurelangovan94@gmail.com>
2024-09-11 01:59:39 +0530

    header Update - Static

--- living-fires/src/App.jsx
+++ living-fires/src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Footer from './components/Footer';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Hero />
+      <Home />
       <Footer />
     </div>
   );
